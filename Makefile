c7663fa4fdbbeaff388e0f19ec310ab98728d78a
Yotam loewenbach <yotam.loewenbach@logz.io>
2023-10-25 14:24:21 +0300

    structure based on `nordstrom/prometheusruleloader`

---
+++ Makefile
@@ -0,0 +1,28 @@
+app_name := prometheusAlertsMigrator
+container_name := prometheusAlertsMigrator
+container_registry := logzio/prometheusAlertsMigrator
+container_release := 5.1
+
+.PHONY: build tag/image push/image clean
+
+build/linux/$(app_name): *.go | build
+	GOOS=linux GOARCH=amd64 go build -o $@ .
+
+build/darwin/$(app_name): *.go | build
+	GOOS=darwin GOARCH=amd64 go build -o $@ .
+
+build/image: build/linux/$(app_name) Dockerfile
+	docker build \
+		-t $(container_name) .
+
+tag/image: build/image
+	docker tag $(container_name) $(container_registry)/$(container_name):$(container_release)
+
+push/image: tag/image
+	docker push $(container_registry)/$(container_name):$(container_release)
+
+build:
+	mkdir -p build/linux build/darwin
+
+clean:
+	rm -rf build
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"github.com/logzio/prometheus-alerts-migrator/controller"
+	"github.com/logzio/prometheus-alerts-migrator/pkg/signals"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/klog"
+	"log"
+	"os"
+	"time"
+
+	"k8s.io/client-go/tools/clientcmd"
+
+	kubeinformers "k8s.io/client-go/informers"
+)
+
+var (
+	// flags general
+	helpFlag            = flag.Bool("help", false, "")
+	configmapAnnotation = flag.String("annotation", "nordstrom.net/prometheus2Alerts", "Annotation that states that this configmap contains prometheus rules.")
+	rulesPath           = flag.String("rulespath", "/rules", "Filepath where the rules from the configmap file should be written, this should correspond to a rule_files: location in your prometheus config.")
+	reloadEndpoint      = flag.String("endpoint", "http://localhost:9090/-/reload/", "Endpoint of the Prometheus reset endpoint (eg: http://prometheus:9090/-/reload).")
+	batchTime           = flag.Int("batchtime", 5, "Time window to batch updates (in seconds, default: 5)")
+	// flags - kubeclient
+	kubeconfigPath = flag.String("kubeconfig", "", "Path to kubeconfig. Required for out of cluster operation.")
+	masterURL      = flag.String("master", "", "The address of the kube api server. Overrides the kubeconfig value, only require for off cluster operation.")
+
+	clientset *kubernetes.Clientset
+	lastSha   string
+)
+
+const (
+	// Resync period for the kube controller loop.
+	resyncPeriod = 30 * time.Minute
+	// A subdomain added to the user specified domain for all services.
+	serviceSubdomain = "svc"
+	// A subdomain added to the user specified dmoain for all pods.
+	podSubdomain = "pod"
+)
+
+func main() {
+	flag.Parse()
+
+	if *helpFlag ||
+		*configmapAnnotation == "" ||
+		*rulesPath == "" ||
+		*reloadEndpoint == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	log.Printf("Rule Updater starting.\n")
+	log.Printf("ConfigMap annotation: %s\n", *configmapAnnotation)
+	log.Printf("Rules location: %s\n", *rulesPath)
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	cfg, err := clientcmd.BuildConfigFromFlags(*masterURL, *kubeconfigPath)
+	if err != nil {
+		log.Fatalf("Error building kubeconfig: %s\n", err.Error())
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
+
+	controller := controller.NewController(kubeClient, kubeInformerFactory.Core().V1().ConfigMaps(), configmapAnnotation, reloadEndpoint, rulesPath)
+
+	// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(stopCh)
+	// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
+	kubeInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
+}
